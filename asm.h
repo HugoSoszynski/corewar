7ce707e8e1b132725b65b14380cf20ee13bbc636
pillon_m <maxime.pillon@epitech.eu>
2016-02-23 15:59:08 +0100

    tot

--- asm.h
+++ asm.h
@@ -5,46 +5,53 @@
 ** Login   <hugo.soszynski@epitech.eu>
 **
 ** Started on  Mon Feb 22 19:35:27 2016 Hugo SOSZYNSKI
-** Last update Tue Feb 23 15:20:08 2016 Hugo SOSZYNSKI
+** Last update Tue Feb 23 15:57:37 2016 
 */
 
 #ifndef		ASM_H_
 # define	ASM_H_
 
-# define	OP_LIVE		(1)
-# define	OP_LD		(2)
-# define	OP_ST		(3)
-# define	OP_ADD		(4)
-# define	OP_SUB		(5)
-# define	OP_AND		(6)
-# define	OP_OR		(7)
-# define	OP_XOR		(8)
-# define	OP_ZJMP		(9)
-# define	OP_LDI		(10)
-# define	OP_STI		(11)
-# define	OP_FORK		(12)
-# define	OP_LLD		(13)
-# define	OP_LLDI		(14)
-# define	OP_LFORK	(15)
-# define	OP_AFF		(16)
+# define	OP_LIVE				(1)
+# define	OP_LD				(2)
+# define	OP_ST				(3)
+# define	OP_ADD				(4)
+# define	OP_SUB				(5)
+# define	OP_AND				(6)
+# define	OP_OR				(7)
+# define	OP_XOR				(8)
+# define	OP_ZJMP				(9)
+# define	OP_LDI				(10)
+# define	OP_STI				(11)
+# define	OP_FORK				(12)
+# define	OP_LLD				(13)
+# define	OP_LLDI				(14)
+# define	OP_LFORK			(15)
+# define	OP_AFF				(16)
 
-typedef struct	s_op
+# define        TYPE_LINE_ERROR			(-1)
+# define	TYPE_LINE_EMPTY			(0)
+# define	TYPE_LINE_NAME			(1)
+# define        TYPE_LINE_COMMENT		(2)
+# define	TYPE_LINE_EXTEND		(3)
+# define	TYPE_LINE_CODE			(4)
+# define        TYPE_LINE_CMD			(5)
+# define	TYPE_LINE_LABEL			(6)
+# define	TYPE_LINE_LABEL_CMD		(7)
+
+typedef struct	s_cmd
 {
   char		opcode;
   char		octet_codage;
-  int		arg1;
-  int		arg2;
-  int		arg3;
-  char		type_arg1;
-  char		type_arg2;
-  char		type_arg3;
-}		t_op;
-
-typedef struct	s_op_tab
-{
-  char		type;
+  int		arg[3];
+  char		type_arg[3];
   char		label[T_LAB];
-  void		*op;
-}		t_op_tab;
+}		t_cmd;
+
+header_t	*create_cor_header(char *,
+				   char **,
+				   int);
+t_cmd		*create_cmd_struct(char *);
+int		check_cmd_line(char *line); /* return 0 reussite et 1 erreur */
+int		choose_line_type(char *line);
 
 #endif		/* !ASM_H_ */
