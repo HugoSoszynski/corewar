bc434eafd073c32b5eac7e1c5239edbdd1bb7168
loens_g <gregoire.loens@epitech.eu>
2016-03-07 19:05:09 +0100

    verification des arguments

--- asm/sources/cmd_line/verif_cmd.c
+++ asm/sources/cmd_line/verif_cmd.c
@@ -13,42 +13,44 @@ char		**set_cmd_part1()
   char		**cmd;
 
   cmd[0] = "live";
-  cmd[1] = "d";
+  cmd[1] = "1d";
   cmd[2] = "ld";
-  cmd[3] = "d/i,r";
+  cmd[3] = "2d/i,r";
   cmd[4] = "st";
-  cmd[5] = "r,d/r";
+  cmd[5] = "3r,d/r";
   cmd[6] = "add";
-  cmd[7] = "r,r,r";
+  cmd[7] = "3r,r,r";
   cmd[8] = "sub";
-  cmd[9] = "r,r,r";
+  cmd[9] = "3r,r,r";
   cmd[10] = "and";
-  cmd[11] = "r/d/i,r/d/i,r";
+  cmd[11] = "3r/d/i,r/d/i,r";
   cmd[12] = "or";
-  cmd[13] = "r/d/i,r/d/i,r";
+  cmd[13] = "3r/d/i,r/d/i,r";
   cmd[14] = "xor";
-  cmd[15] = "r/d/i,r/d/i,r";
+  cmd[15] = "3r/d/i,r/d/i,r";
   cmd[16] = "zjmp";
-  cmd[17] = "d";
+  cmd[17] = "1d";
   cmd[18] = "ldi";
-  cmd[19] = "r/d/i,r/d/i";
+  cmd[19] = "2r/d/i,r/d/i";
   cmd[20] = "sti";
-  cmd[21] = "r,r/d/i,r/d";
+  cmd[21] = "3r,r/d/i,r/d";
   return (set_cmd_part2(cmd));
 }
 
 char		**set_cmd_part2(char **cmd)
 {
   cmd[22] = "fork";
-  cmd[23] = "d";
+  cmd[23] = "1d";
   cmd[24] = "lfork";
-  cmd[25] = "d";
+  cmd[25] = "1d";
   cmd[26] = "lld";
-  cmd[27] = "d/i,r";
+  cmd[27] = "2d/i,r";
   cmd[28] = "lldi";
-  cmd[29] = "r/d/i,r/d,r";
+  cmd[29] = "2r/d/i,r/d,r";
   cmp[30] = "aff";
-  cmp[31] = "r";
+  cmp[31] = "1r";
+  cmp[32] = ".name";
+  cmp[33] = ".comment"
   return (cmd);
 }
 
