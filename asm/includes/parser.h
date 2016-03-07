621c54637a7415a68b664841d827207fbfb6e3d5
loens_g <gregoire.loens@epitech.eu>
2016-03-07 14:22:52 +0100

    ajout dur parser.h

---
+++ asm/includes/parser.h
@@ -0,0 +1,17 @@
+/*
+** parser.h for corewar in /home/loens_g/Rendu/CPE_2015_corewar/asm/includes/
+**
+** Made by Grégoire Loens
+** Login   <loens_g@epitech.net>
+**
+** Started on  Mon Mar  7 13:43:06 2016 Grégoire Loens
+** Last update Mon Mar  7 13:46:38 2016 Grégoire Loens
+*/
+
+#ifndef	PARSER_H_
+# define PARSER_H_
+
+int	my_strlen(char *);
+void	error_message(char *);
+
+#endif	/* !PARSER_H_ */
