ab06a6fa8d2750690069f3c5cd0d0688abb3b5d8
pillon_m <maxime.pillon@epitech.eu>
2016-03-07 20:30:09 +0100

    asm will no finish tomorrow

---
+++ asm/sources/check_exist_cmd.c
@@ -0,0 +1,24 @@
+/*
+** check_exist_cmd.c for  in /home/pillon_m/corewar/CPE_2015_corewar/asm/sources
+** 
+** Made by 
+** Login   <@epitech.net>
+** 
+** Started on  Mon Mar  7 19:23:44 2016 
+** Last update Mon Mar  7 19:47:34 2016 
+*/
+
+int		check_exist_cmd(char *line,
+				char **cmd)
+{
+  int		cpt;
+
+  cpt = 0;
+  while (cpt < 32)
+    {
+      if (my_strcmp(line, cmd[cpt]) == 0)
+	return (cpt);
+      cpt++;
+    }
+  return (-1);
+}
