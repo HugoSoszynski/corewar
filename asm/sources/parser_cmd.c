38ec2269620e392e26ff760d2fa29e88bee947c1
loens_g <gregoire.loens@epitech.eu>
2016-03-07 15:00:16 +0100

    début de parser de commande

--- asm/sources/parser_cmd.c
+++ asm/sources/parser_cmd.c
@@ -5,12 +5,23 @@
 ** Login   <loens_g@epitech.net>
 **
 ** Started on  Mon Mar  7 14:24:56 2016 Grégoire Loens
-** Last update Mon Mar  7 14:28:09 2016 Grégoire Loens
+** Last update Mon Mar  7 14:59:46 2016 Grégoire Loens
 */
 
 #include	"parser.h"
 
 int		verif_cmd_line(int fd, int nbr_line)
 {
-  
+  char		*line;
+
+  while ((line = get_next_line(fd)) != NULL)
+    {
+      if (line = my_isspace(line)) == NULL)
+	{
+	  return(error_message_parser("Syntax error line ", nbr_line));
+	}
+      if (verif_nbr_cmd == -1)
+	{
+	}
+    }
 }
