723c24123b759d6721f619ccca65b729d077696a
loens_g <gregoire.loens@epitech.eu>
2016-03-08 15:26:50 +0100

    on merge OKLM

--- asm/sources/parser_cmd.c
+++ asm/sources/parser_cmd.c
@@ -5,10 +5,35 @@
 ** Login   <loens_g@epitech.net>
 **
 ** Started on  Mon Mar  7 14:24:56 2016 Grégoire Loens
-** Last update Mon Mar  7 16:09:42 2016 
+** Last update Tue Mar  8 15:16:51 2016 Grégoire Loens
 */
 
 #include	"parser.h"
+#include	"asm.h"
+
+int		type_of_cmd(char *line)
+{
+  if (line[0] == '.')
+    {
+      if (gst_name(line) == 0)
+	return (TYPE_LINE_NAME);
+      else if (gst_comment(line) == 0)
+	return (TYPE_LINE_COMMENT);
+      else if (gst_code(line) == 0)
+	return (TYPE_LINE_CODE);
+      else if (gst_extend(line) == 0)
+	return (TYPE_LINE_EXTEND);
+    }
+  else
+    {
+      if (gst_label(line) == 1)
+	return(TYPE_LINE_LABEL);
+      else if (gst_label(line) == 2)
+	return (TYPE_LINE_LABEL_CMD);
+      else if (gst_cmd(line) == 0)
+	return (TYPE_LINE_CMD);
+    }
+}
 
 int		verif_cmd_line(int fd, int nbr_line)
 {
