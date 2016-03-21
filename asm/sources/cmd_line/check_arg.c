fe77cc9343cc57f18e59e69ee63e4973e0a3b0a0
loens_g <gregoire.loens@epitech.eu>
2016-03-21 21:21:44 +0100

    on passe au debug du parsing

--- asm/sources/cmd_line/check_arg.c
+++ asm/sources/cmd_line/check_arg.c
@@ -1,11 +1,11 @@
 /*
 ** check_arg.c for  in /home/pillon_m/corewar/CPE_2015_corewar/asm/sources/cmd_line
-** 
-** Made by 
+**
+** Made by
 ** Login   <@epitech.net>
-** 
-** Started on  Mon Mar 21 17:40:54 2016 
-** Last update Mon Mar 21 17:55:05 2016 
+**
+** Started on  Mon Mar 21 17:40:54 2016
+** Last update Mon Mar 21 18:24:39 2016 Grégoire Loens
 */
 
 #include	"parser.h"
@@ -13,14 +13,62 @@
 
 int		arg_direct(char *arg_file)
 {
+  int		cpt;
+
+  cpt = 1;
+  if (arg_file[cpt - 1] != '%')
+    return (-1);
+  if (arg_file[cpt] == '\0')
+    return (-1);
+  while (arg_file[cpt] != 0)
+    {
+      if(arg_file[cpt] < '0' && arg_file[cpt] > '9')
+	return (-1);
+      else
+	cpt++;
+    }
+  return (0);
 }
 
 int		arg_indirect(char *arg_file)
 {
+  int		cpt;
+
+  cpt = 0;
+  if (arg_file[cpt] == '%')
+    {
+      if (arg_file[cpt + 1] != ':' || arg_file[cpt + 2] == '\0')
+	return (-1);
+      if (arg_register(arg_file) == 0)
+	return (0);
+      if (my_issalpha(arg_file + 2) == 0)
+	return (0);
+      if (my_isnum(arg_file + 2) == 0)
+	return (0);
+      return (-1);
+    }
+  else
+    if (my_isnum(arg_file) == 1)
+	return(0);
 }
 
 int		arg_register(char *arg_file)
 {
+  int		cpt;
+
+  cpt = 0;
+  if (arg_file[cpt] != 'r')
+    return (-1);
+  else
+    {
+      cpt++;
+      if ((((arg_file[cpt] > '1' && arg_file[cpt] < '7') && arg_file[cpt] <=
+	    '9') && arg_file[cpt + 1] == '\0') || (arg_file[cpt] == '1' &&
+	   (arg_file[cpt + 1] >= '0' && arg_file[cpt + 1] <= "6")))
+	return (0);
+      else
+	return (-1);
+    }
 }
 
 int		check_is_type(char *arg_file, char *type)
@@ -53,4 +101,3 @@ int             check_one_arg(char *arg_file, char *arg_cmd)
     }
   return (-1);
 }
-
--- asm/sources/cmd_line/check_label_cmd.c
+++ asm/sources/cmd_line/check_label_cmd.c
@@ -1,13 +1,23 @@
 /*
 ** check_label_cmd.c for  in /home/pillon_m/corewar/CPE_2015_corewar/asm/sources/cmd_line
-** 
-** Made by 
+**
+** Made by
 ** Login   <@epitech.net>
-** 
-** Started on  Tue Mar  8 16:21:23 2016 
-** Last update Tue Mar  8 16:21:46 2016 
+**
+** Started on  Tue Mar  8 16:21:23 2016
+** Last update Mon Mar 21 21:20:00 2016 Grégoire Loens
 */
 
 int			check_label_cmd(char *line)
 {
+  int			cpt;
+
+  cpt = 0;
+  while (line[cpt] != '\0' && line[cpt] != ' ')
+    cpt++;
+  if (line[cpt + 1] == '\0')
+    return (-1);
+  if (check_cmd(line + cpt + 1) == -1)
+    return (-1);
+  return (0);
 }
