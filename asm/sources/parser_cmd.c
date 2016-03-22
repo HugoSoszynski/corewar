2e0cf955e72fff8ccf623dc0d60b5b3d32c021d5
loens_g <gregoire.loens@epitech.eu>
2016-03-22 09:09:02 +0100

    petit merge a l'ancienne

--- asm/sources/parser_cmd.c
+++ asm/sources/parser_cmd.c
@@ -5,7 +5,7 @@
 ** Login   <loens_g@epitech.net>
 **
 ** Started on  Mon Mar  7 14:24:56 2016 Grégoire Loens
-** Last update Mon Mar 21 19:14:18 2016 
+** Last update Mon Mar 21 21:20:49 2016 Grégoire Loens
 */
 
 #include	"../includes/parser.h"
@@ -58,23 +58,11 @@ int		parsing(char *line)
   if (type == TYPE_LINE_CMD)
     if (check_cmd(line) == -1)
       return(error_message_parser("Syntax error line ", nbr_line));
-  if (type == TYPE_LINE_LABEL)
-    if (check_label(line) == -1)
-      return(error_message_parser("Syntax error line ", nbr_line));
-  if (type == TYPE_LINE_LABER_CMD)
+   if (type == TYPE_LINE_LABER_CMD)
     if (check_label_cmd(line) == -1)
       return(error_message_parser("Syntax error line ", nbr_line));
   return (type);
 }
-
-  /*if (verif_nbr_cmd(line) == -1)
-    return (error_message_parser("Syntax error line ", nbr_line));
-  if (verif_nbr_arg(line) == -1)
-    return (error_message_parser("Syntax error line ", nbr_line));
-  if (command_with_arg(line) == -1)
-    return (error_message_parser("Syntax error line ", nbr_line));
-  if (use_of_arg(line) == -1)
-  return (error_message_parser("Syntax error line ", nbr_line));  */
 
 int		verif_cmd_line(int fd, int nbr_line)
 {
@@ -83,7 +71,6 @@ int		verif_cmd_line(int fd, int nbr_line)
 
   while ((line = get_next_line(fd)) != NULL)
     {
-
       type = parsing(line);
     }
 }
