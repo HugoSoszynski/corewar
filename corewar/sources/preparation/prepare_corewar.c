87dd20f5653f2c38d125860922249ad2fd05c250
corsin_a <sylvain.corsini@epitech.eu>
2016-03-23 01:06:01 +0100

    actual cycle init à 1 au lieu de 0

--- corewar/sources/preparation/prepare_corewar.c
+++ corewar/sources/preparation/prepare_corewar.c
@@ -5,7 +5,7 @@
 ** Login   <sylvain.corsini@epitech.eu>
 **
 ** Started on  Tue Mar 22 15:44:55 2016 corsin_a
-** Last update Tue Mar 22 23:34:11 2016 corsin_a
+** Last update Tue Mar 22 23:37:11 2016 corsin_a
 */
 
 #include		<stddef.h>
@@ -18,7 +18,10 @@ static t_process_list	*create_process_list(t_champion		*champion)
   int			cpt;
 
   if ((process_list = malloc(sizeof(t_process_list))) == NULL)
-    return (error_message("Can't perform malloc"));
+    {
+      error_message("Can't perform malloc");
+      return (NULL);
+    }
   process_list->cycle = 0;
   process_list->current.opcode = 0;
   process_list->process.carry = 0;
@@ -77,7 +80,7 @@ static void		prepare_mem(t_corewar			*corewar)
 int			prepare_corewar(t_corewar		*corewar)
 {
   corewar->cycle_to_die = CYCLE_TO_DIE;
-  corewar->actual_cycle = 0;
+  corewar->actual_cycle = 1;
   corewar->live_nb = 0;
   corewar->cycle_passed = 0;
   if (prepare_process_list(corewar) != SUCCESS)
