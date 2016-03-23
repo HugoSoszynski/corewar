bdcee82341009ba1239d8093f682a1baa8ec1cd4
loens_g <gregoire.loens@epitech.eu>
2016-03-23 14:42:46 +0100

    initialisation des listes chaînéeception

--- asm/sources/init_stock.c
+++ asm/sources/init_stock.c
@@ -5,11 +5,24 @@
 ** Login   <loens_g@epitech.net>
 **
 ** Started on  Wed Mar 23 11:21:21 2016 Grégoire Loens
-** Last update Wed Mar 23 11:34:21 2016 Grégoire Loens
+** Last update Wed Mar 23 12:19:35 2016 Grégoire Loens
 */
 
 #include	<stdlib.h>
 #include	"asm.h"
+
+t_cmd		*init_first_cmd()
+{
+  t_list	*stock_cmd;
+
+  if ((stock_cmd = malloc(sizeof(t_cmd))) == NULL)
+    return (NULL);
+  stock_cmd->head = init_cmd();
+  stock_cmd->head->def = init_pile();
+  stock_cmd->head->call = init_pile();
+  stock_cmd->head->next = NULL;
+  return (stock_cmd);
+}
 
 t_cmd		*init_cmd()
 {
@@ -18,14 +31,17 @@ t_cmd		*init_cmd()
   if ((stock_cmd = malloc(sizeof(t_cmd))) == NULL)
     return (NULL);
   stock_cmd->next = NULL;
-  return (stock_cmd)
+  return (stock_cmd);
 }
 
-t_cmd		add_new_cmd(t_cmd *stock_cmd)
+t_list		add_cmd(t_list *stock_cmd)
 {
   t_cmd		*new_stock_cmd;
 
   new_stock_cmd = init_cmd();
+  stock_cmd = stock_cmd->head->next;
+  while (stock_cmd->next != NULL)
+    stock_cmd = stock_cmd->next;
   stock_cmd->next = new_stock_cmd;
   return (stock_cmd);
 }
