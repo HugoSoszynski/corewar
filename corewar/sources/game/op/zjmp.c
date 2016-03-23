9305f25aa311c51db7e8f4eb0b3c5187d47c13ce
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2016-03-23 16:10:27 +0100

    Fonction zjmp ok (ne pas oublier de ne pas modifier le pc dans le jeu)

--- corewar/sources/game/op/zjmp.c
+++ corewar/sources/game/op/zjmp.c
@@ -5,7 +5,7 @@
 ** Login   <sylvain.corsini@epitech.eu>
 **
 ** Started on  Wed Mar 23 03:16:32 2016 corsin_a
-** Last update Wed Mar 23 03:28:20 2016 corsin_a
+** Last update Wed Mar 23 16:04:37 2016 Hugo SOSZYNSKI
 */
 
 #include	"corewar.h"
@@ -18,7 +18,7 @@ int		check_op_zjmp(char	opcode)
 }
 
 void		op_zjmp(t_corewar	*corewar,
-			t_process_list	*process_list)
+			t_process_list	*curent)
 {
-
+  current->process.pc += current->instruction.arg[0];
 }
