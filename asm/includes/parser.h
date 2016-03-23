0a56719ecfe3568a903874d7577a9f150970ce8e
loens_g <gregoire.loens@epitech.eu>
2016-03-23 10:43:48 +0100

    on prtège tous les strdup du tableau lolololololololololololololol

--- asm/includes/parser.h
+++ asm/includes/parser.h
@@ -5,7 +5,7 @@
 ** Login   <loens_g@epitech.net>
 **
 ** Started on  Mon Mar  7 13:43:06 2016 Grégoire Loens
-** Last update Wed Mar 23 10:30:57 2016 Grégoire Loens
+** Last update Wed Mar 23 10:43:02 2016 Grégoire Loens
 */
 
 #ifndef	PARSER_H_
@@ -39,6 +39,7 @@ int	start_word(char *, int);
 char	*my_getword(char *, int);
 char	**set_cmd_part1();
 char	**set_cmd_part2(char **);
+char	**set_cmd_part3(char **);
 int	my_isnum(char *);
 int	my_isalpha(char *);
 int	error_message_parser(char *, int);
--- asm/sources/cmd_line/tab_cmd.c
+++ asm/sources/cmd_line/tab_cmd.c
@@ -5,28 +5,71 @@
 ** Login   <@epitech.net>
 **
 ** Started on  Tue Mar  8 16:05:30 2016
-** Last update Wed Mar 23 04:50:38 2016 
+** Last update Wed Mar 23 04:50:38 2016
 */
 
 #include	<stdlib.h>
 #include	"asm.h"
 #include	"parser.h"
 
+char		**set_cmd_part3(char **cmd)
+{
+  if ((cmd[20] = my_strdup("sti")) == NULL)
+    return (NULL);
+  if ((cmd[21] = my_strdup("r,r/d/i,r/d")) == NULL)
+    return (NULL);
+  if ((cmd[22] = my_strdup("fork")) == NULL)
+    return (NULL);
+  if ((cmd[23] = my_strdup("d")) == NULL)
+    return (NULL);
+  if ((cmd[24] = my_strdup("lfork")) == NULL)
+    return (NULL);
+  if ((cmd[25] = my_strdup("d")) == NULL)
+    return (NULL);
+  if ((cmd[26] = my_strdup("lld")) == NULL)
+    return (NULL);
+  if ((cmd[27] = my_strdup("d/i,r")) == NULL)
+    return (NULL);
+  if ((cmd[28] = my_strdup("lldi")) == NULL)
+    return (NULL);
+  if ((cmd[29] = my_strdup("r/d/i,r/d,r")) == NULL)
+    return (NULL);
+  if ((cmd[30] = my_strdup("aff")) == NULL)
+    return (NULL);
+  if ((cmd[31] = my_strdup("r")) == NULL)
+    return (NULL);
+  if ((cmd[32] = my_strdup(".name")) == NULL)
+    return (NULL);
+  if ((cmd[33] = my_strdup(".comment")) == NULL)
+    return (NULL);
+  return (cmd);
+}
+
 char		**set_cmd_part2(char **cmd)
 {
-  cmd[22] = my_strdup("fork");
-  cmd[23] = my_strdup("d");
-  cmd[24] = my_strdup("lfork");
-  cmd[25] = my_strdup("d");
-  cmd[26] = my_strdup("lld");
-  cmd[27] = my_strdup("d/i,r");
-  cmd[28] = my_strdup("lldi");
-  cmd[29] = my_strdup("r/d/i,r/d,r");
-  cmd[30] = my_strdup("aff");
-  cmd[31] = my_strdup("r");
-  cmd[32] = my_strdup(".name");
-  cmd[33] = my_strdup(".comment");
-  return (cmd);
+  if ((cmd[9] = my_strdup("r,r,r")) == NULL)
+    return (NULL);
+  if  ((cmd[10] = my_strdup("and")) == NULL)
+    return (NULL);
+  if ((cmd[11] = my_strdup("r/d/i,r/d/i,r")) == NULL)
+    return (NULL);
+  if ((cmd[12] = my_strdup("or")) == NULL)
+    return (NULL);
+  if ((cmd[13] = my_strdup("r/d/i,r/d/i,r")) == NULL)
+    return (NULL);
+  if ((cmd[14] = my_strdup("xor")) == NULL)
+    return (NULL);
+  if ((cmd[15] = my_strdup("r/d/i,r/d/i,r")) == NULL)
+    return (NULL);
+  if ((cmd[16] = my_strdup("zjmp")) == NULL)
+    return (NULL);
+  if ((cmd[17] = my_strdup("d")) == NULL)
+    return (NULL);
+  if ((cmd[18] = my_strdup("ldi")) == NULL)
+    return (NULL);
+  if ((cmd[19] = my_strdup("r/d/i,r/d,r")) == NULL)
+    return (NULL);
+  return (set_cmd_part3(cmd));
 }
 
 char		**set_cmd_part1()
@@ -34,27 +77,23 @@ char		**set_cmd_part1()
   char		**cmd;
 
   cmd = malloc(sizeof(char *) * 35);
-  cmd[0] = my_strdup("live");
-  cmd[1] = my_strdup("d");
-  cmd[2] = my_strdup("ld");
-  cmd[3] = my_strdup("d/i,r");
-  cmd[4] = my_strdup("st");
-  cmd[5] = my_strdup("r,d/r");
-  cmd[6] = my_strdup("add");
-  cmd[7] = my_strdup("r,r,r");
-  cmd[8] = my_strdup("sub");
-  cmd[9] = my_strdup("r,r,r");
-  cmd[10] = my_strdup("and");
-  cmd[11] = my_strdup("r/d/i,r/d/i,r");
-  cmd[12] = my_strdup("or");
-  cmd[13] = my_strdup("r/d/i,r/d/i,r");
-  cmd[14] = my_strdup("xor");
-  cmd[15] = my_strdup("r/d/i,r/d/i,r");
-  cmd[16] = my_strdup("zjmp");
-  cmd[17] = my_strdup("d");
-  cmd[18] = my_strdup("ldi");
-  cmd[19] = my_strdup("r/d/i,r/d,r");
-  cmd[20] = my_strdup("sti");
-  cmd[21] = my_strdup("r,r/d/i,r/d");
+  if ((cmd[0] = my_strdup("live")) == NULL)
+    return (NULL);
+  if ((cmd[1] = my_strdup("d")) == NULL)
+    return (NULL);
+  if ((cmd[2] = my_strdup("ld")) == NULL)
+    return (NULL);
+  if ((cmd[3] = my_strdup("d/i,r")) == NULL)
+    return (NULL);
+ if ((cmd[4] = my_strdup("st")) == NULL)
+    return (NULL);
+ if ((cmd[5] = my_strdup("r,d/r")) == NULL)
+    return (NULL);
+ if ((cmd[6] = my_strdup("add")) == NULL)
+    return (NULL);
+ if ((cmd[7] = my_strdup("r,r,r")) == NULL)
+    return (NULL);
+ if ((cmd[8] = my_strdup("sub")) == NULL)
+    return (NULL);
   return (set_cmd_part2(cmd));
 }
