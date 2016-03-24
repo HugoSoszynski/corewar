3533b664e15c189464f0f19df65f0419fd5aca04
loens_g <gregoire.loens@epitech.eu>
2016-03-24 10:59:01 +0100

    fonction de stockage des labels

---
+++ asm/sources/stock_arg/stock_pile.c
@@ -0,0 +1,86 @@
+/*
+** stock_pile.c for corewar in /home/loens_g/Rendu/CPE_2015_corewar/asm/sources/pile/
+**
+** Made by Grégoire Loens
+** Login   <loens_g@epitech.net>
+**
+** Started on  Wed Mar 23 01:30:46 2016 Grégoire Loens
+** Last update Wed Mar 23 11:55:36 2016 Grégoire Loens
+*/
+
+#include	<stdlib.h>
+#include	<stddef.h>
+#include	"../../includes/pile_label.h"
+
+t_pile		*init_pile(void)
+{
+  t_pile	*label;
+
+  if ((label = malloc(sizeof(t_pile))) == NULL)
+    return (NULL);
+  label->next = NULL;
+  label->label_name = NULL;
+  return (label);
+}
+
+t_pile		*add_label(t_pile *label)
+{
+  t_pile	*new_label;
+
+  new_label = init_pile();
+  while (label->next != NULL)
+    label = label->next;
+  label->next = new_label;
+  label->next->label_name = NULL;
+  return (label);
+}
+
+t_pile		*stock_pile_for_def(t_pile *def, char *line)
+{
+  int		cpt;
+
+  cpt = 0;
+  while (def->next != NULL)
+    def = def->next;
+  if (def->label_name != NULL)
+    {
+      def = add_label(def);
+      def = def->next;
+    }
+  while(line[cpt] != ':')
+    cpt++;
+  if ((def->label_name = malloc(sizeof(char) * cpt + 1)) == NULL)
+    return (NULL);
+  cpt = 0;
+  while (line[cpt] != ':')
+    {
+      def->label_name[cpt] = line[cpt];
+      cpt++;
+    }
+  return (def);
+}
+
+t_pile		*stock_pile_for_call(t_pile *call, char *arg)
+{
+  int		cpt;
+
+  cpt = 0;
+  while (arg[cpt] != 0)
+   cpt++;
+  while(call->next != NULL)
+    call = call->next;
+  if (call->label_name != NULL)
+    {
+      call = add_label(call);
+      call = call->next;
+    }
+  if ((call->label_name = malloc(sizeof(char) * cpt + 1)) == NULL)
+    return (NULL);
+  cpt = 0;
+  while (arg[cpt] != 0)
+    {
+      call->label_name[cpt] = arg[cpt];
+      cpt++;
+    }
+  return (call);
+}
