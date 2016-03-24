dec63a4ec2de6646cc133427ffc6ddfa9fe56777
loens_g <gregoire.loens@epitech.eu>
2016-03-25 00:41:23 +0100

    gestion des label cod lol

---
+++ asm/sources/stock_arg/calc_octet.c
@@ -0,0 +1,53 @@
+/*
+** calc_octet.c for corewar in /home/loens_g/Rendu/CPE_2015_corewar/asm/sources/stock_arg/
+**
+** Made by Grégoire Loens
+** Login   <loens_g@epitech.net>
+**
+** Started on  Thu Mar 24 23:20:29 2016 Grégoire Loens
+** Last update Fri Mar 25 00:38:20 2016 Grégoire Loens
+*/
+
+#include	"asm.h"
+#include	"pile_label.h"
+
+int		calc_with_def(t_cmd *call, t_cmd *def, t_cmd *arg)
+{
+  while (arg->nb_line != def->def->nb_line && arg != NULL && def->def != NULL)
+    arg = arg->next;
+  while (arg->nb_line < call->call->nb_line && arg != NULL && def->def != NULL)
+    {
+      nb_octet += add_octet(arg);
+      arg = arg->next;
+    }
+  nb_octet -= nb_octet * 2;
+  return (nb_octet);
+}
+
+int		calc_with_call(t_cmd *call, t_cmd *def, t_cmd *arg)
+{
+  while (arg->nb_line != call->call->nb_line && arg != NULL
+	 && call->call != NULL)
+    arg = arg->next;
+  while (arg->nb_line <= def->nb_line && arg != NULL && def->def != NULL)
+    {
+      nb_octet += add_octet(arg);
+      arg = arg->next;
+    }
+  return (nb_octet);
+}
+
+int		add_octet(t_cmd *arg)
+{
+  int		cpt;
+  int		nb_octet;
+
+  nb_octet = 0;
+  cpt = 0;
+  while (arg->arg[cpt] != 0)
+    {
+      nb_octet += arg->arg[pt];
+      cpt++;
+    }
+  return (nb_octet);
+}
---
+++ asm/sources/stock_arg/set_octet_label.c
@@ -0,0 +1,78 @@
+/*
+** set_octet_label.c for corewar in /home/loens_g/Rendu/CPE_2015_corewar/asm/sources/stock_arg/
+**
+** Made by Grégoire Loens
+** Login   <loens_g@epitech.net>
+**
+** Started on  Thu Mar 24 21:41:06 2016 Grégoire Loens
+** Last update Fri Mar 25 00:40:54 2016 Grégoire Loens
+*/
+
+#include	"../../includes/asm.h"
+#include	"../../includes/pile_label.h"
+
+int		calc_octet(t_cmd *call, t_cmd *def, t_cmd *arg)
+{
+  int		nb_octet;
+
+  nb_octet = 0;
+  if (call->call->nb_line > def->def->nb_line && call->call != NULL
+      && def->def != NULL)
+    nb_octet = calc_with_def(call, def, arg);
+  else if (call->call->nb_line < def->def->nb_line && call->call != NULL
+	   && def->def != NULL)
+    nb_octet = calc_with_call(call, def, arg);
+  return (nb_octet);
+}
+
+void		search_def(t_cmd *call, t_cmd *def)
+{
+  while(strcmpend(call->call->label, def->def->label) != 1)
+    def->def = def->def->next;
+}
+
+void		set_arg(t_cmd *call, t_cmd *def, t_cmd *arg)
+{
+  int		rpr;
+  int		nb_octet;
+
+  rpr = -1;
+  nb_octet = calc_octet(call, def, arg);
+  while (arg->nb_line != call->call->nb_line && arg != NULL
+	 && call->call != NULL)
+    arg = arg->next;
+  while (arg->type_arg[++rpr] != 0)
+    {
+      if (arg->type_arg[rpr] == 12 && arg->arg[rpr] == 0)
+	{
+	  arg->arg[rpr] = nb_octet;
+	  arg->type_arg[rpr] = 2;
+	  return ;
+	}
+      else if (arg->type_arg[rpr] == 14 && arg->arg[rpr] == 0)
+	{
+	  arg->arg[rpr] = nbr_octet;
+	  arg->type_arg[rpr] = 4;
+	  return ;
+	}
+    }
+}
+
+t_cmd		set_octet_label(t_cmd *cmd)
+{
+  t_cmd		*call;
+  t_cmd		*def;
+  t_cmd		*arg;
+
+  call = cmd->head;
+  def  = cmd->head;
+  arg = cmd->head;
+  while(call->call != NULL);
+    {
+      search_def(call, def);
+      set_arg(call, def, arg);
+      def = cmd->head;
+      arg = cmd->head;
+      call->call->next;
+    }
+}
